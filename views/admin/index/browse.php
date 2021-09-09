4d01b1a67e6df6efdd4d31c3ae060296b5c941a1
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-09-09 22:04:34 +0200

    Changed strings, added fields, modified table style

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -10,11 +10,11 @@ echo head($head);
 ?>
 <?php echo flash(); ?>
 
-<a class="add-page button green" href="<?php echo html_escape(url('email-users/index/send')); ?>"><?php echo __('Send a new e-mail'); ?></a>
+<a class="add-page button green" href="<?php echo html_escape(url('email-users/index/edit')); ?>"><?php echo __('Compose new e-mail'); ?></a>
 <?php if (!has_loop_records('email_users_message')): ?>
-    <p><?php echo __('No e-mail message has been sent yet.'); ?> <a href="<?php echo html_escape(url('email-users/index/send')); ?>"><?php echo __('Send a new e-mail'); ?></a>.</p>
+    <p><?php echo __('No e-mail message has been created or sent yet.'); ?> <a href="<?php echo html_escape(url('email-users/index/edit')); ?>"><?php echo __('Compose new e-mail'); ?></a>.</p>
 <?php else: ?>
-	<p><?php echo __('Here are the e-mail messages already sent to users of this site.'); ?></p>
+	<p><?php echo __('Here are all e-mail messages already created and/or sent to users of this site. If a message has not been sent, click on <b>Edit</b> button to edit and send it. Click on the message <b>Subject</b> to see all details of that message.'); ?></p>
 	<table class="full">
 		<thead>
 			<tr>
@@ -22,11 +22,12 @@ echo head($head);
 					echo browse_sort_links(
 						array(
 							__('Subject') => 'subject',
-							__('Sender') => 'sender',							
-							__('Recipients') => 'recipients',							
+							__('Creator') => 'sender',							
+							__('Created') => 'created',
 							__('Type') => 'type',
 							__('Priority') => 'priority',
-							__('Datetime sent') => 'datetime_sent'
+							__('Recipients') => 'recipients',							
+							__('Sent') => 'sent'
 						),
 						array('link_tag' => 'th scope="col"', 'list_tag' => '')
 					);
@@ -36,23 +37,34 @@ echo head($head);
 		<tbody>
 		<?php foreach (loop('email_users_message') as $emailUsersMessage): ?>
 			<tr>
-				<td>
+				<td class="centred-left">
 					<span class="title">
 						<a href="<?php echo html_escape(record_url('email_users_message', 'show')); ?>">
 							<?php echo $emailUsersMessage->subject; ?>
 						</a>
 					</span>
 				</td>
-				<td>
+				<td class="centred">
 					<?php 
 						$user = get_record_by_id('User', $emailUsersMessage->sender_id);
 						echo $user->name;
 					?>
 				</td>
-				<td><?php echo $emailUsersMessage->getRecipientsCount($emailUsersMessage->id); ?></td>
-				<td><?php echo ($emailUsersMessage->html == 1 ? __('HTML') : __('Text')); ?></td>
-				<td><?php echo ($emailUsersMessage->priority == 1 ? __('High') : __('Normal')); ?></td>
-				<td><?php echo html_escape($emailUsersMessage->datetime_sent); ?></td>
+				<td class="centred"><?php echo html_escape($emailUsersMessage->created); ?></td>
+				<td class="centred"><?php echo ($emailUsersMessage->html == 1 ? __('HTML') : __('Text')); ?></td>
+				<td class="centred"><?php echo ($emailUsersMessage->priority == 1 ? __('High') : __('Normal')); ?></td>
+				<td class="centred">
+					<?php echo ($emailUsersMessage->sent != '' ? $emailUsersMessage->getRecipientsCount($emailUsersMessage->id) : '&nbsp;'); ?>
+				</td>
+				<td class="centred">
+					<?php 
+						if ($emailUsersMessage->sent != '') {
+							echo html_escape($emailUsersMessage->sent);
+						} else {
+							echo '<a class="full-width-mobile button blue" href="' . html_escape(url('email-users/index/edit/id/') . $emailUsersMessage->id) .'">' . __('Edit'). '</a>';
+						}
+					?>
+				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>

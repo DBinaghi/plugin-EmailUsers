e8975b51fa3df6f547a5b5db8d5ed5bd2acc0183
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-09-15 14:06:28 +0200

    Added pagination and styling

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -10,14 +10,20 @@
 	}
 	echo head($head);
 ?>
+
 <?php echo flash(); ?>
 
 <a class="add-page button green" href="<?php echo html_escape(url('email-users/index/edit')); ?>"><?php echo __('Compose new e-mail'); ?></a>
 <?php if (!has_loop_records('email_users_message')): ?>
-    <p><?php echo __('No e-mail message has been created or sent yet.'); ?> <a href="<?php echo html_escape(url('email-users/index/edit')); ?>"><?php echo __('Compose new e-mail'); ?></a>.</p>
+    <p>
+		<?php echo __('No e-mail message has been created or sent yet.'); ?> <a href="<?php echo html_escape(url('email-users/index/edit')); ?>"><?php echo __('Compose new e-mail'); ?></a>.
+	</p>
 <?php else: ?>
-	<p><?php echo __('Here are all e-mail messages already created and/or sent to users of this site. If a message has not been sent, click on <b>Edit</b> button to edit and send it. Click on a message <b>Subject</b> to see all details of that message.'); ?></p>
-	<table class="full">
+	<p>
+		<?php echo __('Here are all e-mail messages already created and/or sent to users of this site. If a message has not been sent, click on <b>Edit</b> button to edit and send it. Click on a message <b>Subject</b> to see all details of that message.'); ?>
+	</p>
+    <?php echo pagination_links(); ?>
+	<table id="email-users-messages-list">
 		<thead>
 			<tr>
 				<?php 
@@ -37,36 +43,37 @@
 			</tr>
 		</thead>
 		<tbody>
-		<?php foreach (loop('email_users_message') as $emailUsersMessage): ?>
-			<tr>
-				<td class="centred-left">
-					<a href="<?php echo html_escape(record_url('email_users_message', 'show')); ?>">
-						<?php echo $emailUsersMessage->subject; ?>
-					</a>
-				</td>
-				<td class="centred">
-					<?php 
-						$user = get_record_by_id('User', $emailUsersMessage->sender_id);
-						echo $user->name;
-					?>
-				</td>
-				<td class="centred"><?php echo html_escape($emailUsersMessage->created); ?></td>
-				<td class="centred"><?php echo ($emailUsersMessage->html == 1 ? __('HTML') : __('Text')); ?></td>
-				<td class="centred"><?php echo ($emailUsersMessage->priority == 1 ? __('High') : __('Normal')); ?></td>
-				<td class="centred">
-					<?php echo ($emailUsersMessage->sent != '' ? $emailUsersMessage->getRecipientsCount($emailUsersMessage->id) : '&nbsp;'); ?>
-				</td>
-				<td class="centred">
-					<?php 
-						if ($emailUsersMessage->sent != '') {
-							echo html_escape($emailUsersMessage->sent);
-						} else {
-							echo '<a class="full-width-mobile button blue" href="' . html_escape(url('email-users/index/edit/id/') . $emailUsersMessage->id) .'">' . __('Edit'). '</a>';
-						}
-					?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
+			<?php $key = 0; ?>
+			<?php foreach (loop('email_users_message') as $emailUsersMessage): ?>
+				<tr class="<?php echo (++$key%2 == 1 ? 'odd' : 'even'); ?>">
+					<td class="centred-left">
+						<a href="<?php echo html_escape(record_url('email_users_message', 'show')); ?>">
+							<?php echo $emailUsersMessage->subject; ?>
+						</a>
+					</td>
+					<td class="centred">
+						<?php 
+							$user = get_record_by_id('User', $emailUsersMessage->sender_id);
+							echo $user->name;
+						?>
+					</td>
+					<td class="centred"><?php echo html_escape($emailUsersMessage->created); ?></td>
+					<td class="centred"><?php echo ($emailUsersMessage->html == 1 ? __('HTML') : __('Text')); ?></td>
+					<td class="centred"><?php echo ($emailUsersMessage->priority == 1 ? __('High') : __('Normal')); ?></td>
+					<td class="centred">
+						<?php echo ($emailUsersMessage->sent != '' ? $emailUsersMessage->getRecipientsCount($emailUsersMessage->id) : '&nbsp;'); ?>
+					</td>
+					<td class="centred">
+						<?php 
+							if ($emailUsersMessage->sent != '') {
+								echo html_escape($emailUsersMessage->sent);
+							} else {
+								echo '<a class="full-width-mobile button blue" href="' . html_escape(url('email-users/index/edit/id/') . $emailUsersMessage->id) .'">' . __('Edit'). '</a>';
+							}
+						?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
 		</tbody>
 	</table>
 <?php endif; ?>
